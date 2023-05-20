6170502033f07c61612d87768b239c2c8774730d
FerhatYesilkaya <ferhat-20001@hotmail.de>
2023-05-20 23:10:09 +0200

    Revert "Revert "Merge branch 'FerhatDev' of https://github.com/Rindersteak/JudoKampfFront into FerhatDev""
    This reverts commit de699da7df99b26d25a98c0e58c216977464c2ba.

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,10 +1,9 @@
-// HomePage.tsx
-
-import React, { CSSProperties } from 'react';
-import logo from '../../img/kadokan_logo.svg';
-import './HomePage.css';
+import React, { CSSProperties } from 'react'; // React und CSSProperties importieren
+import logo from '../../img/kadokan_logo.svg'; // Importieren des Logos als Bild
+import './HomePage.css'; // Importieren von Styling für HomePage
 
 interface HomePageProps {
+<<<<<<< HEAD
     onOpenFighterManager: () => void;
     onOpenTournamentForm: () => void;
 }
@@ -12,22 +11,34 @@ interface HomePageProps {
 
 
 const HomePage: React.FC<HomePageProps> = ({ onOpenFighterManager, onOpenTournamentForm }) => {
+=======
+    onOpenFighterManager: () => void; // Props-Typ definieren, der eine Funktion erwartet
+}
+
+const HomePage: React.FC<HomePageProps> = ({ onOpenFighterManager }) => { // Deklaration der HomePage-Komponente als Funktionskomponente
+>>>>>>> b7a44c0172934c6eb2371e13f402cfa03397d2a7
     return (
-        <div className="content">
-            <div className="top-banner">
-                <img src={logo} alt="Logo" className="logo" />
-                <h1 style={{ textAlign: 'center' }}>Willkommen bei<br />kodokan</h1>
+        <div className="content"> {/* Div-Container mit der Klasse "content" */}
+            <div className="top-banner"> {/* Div-Container für den oberen Banner */}
+                <img src={logo} alt="Logo" className="logo" /> {/* Logo-Bild */}
+                <h1 style={{ textAlign: 'center' }}>Willkommen bei<br />kodokan</h1> {/* Überschrift für die Startseite */}
             </div>
+<<<<<<< HEAD
             <div className="lower-container">
                 <div onClick={onOpenTournamentForm} className="left-container">
                     <h1>Neues Turnier<br />anlegen</h1>
+=======
+            <div className="lower-container"> {/* Div-Container für den unteren Bereich */}
+                <div onClick={onOpenFighterManager} className="left-container"> {/* Div-Container für den linken Bereich */}
+                    <h1>Neues Turnier<br />anlegen</h1> {/* Überschrift für die Option, ein neues Turnier anzulegen */}
+>>>>>>> b7a44c0172934c6eb2371e13f402cfa03397d2a7
                 </div>
-                <div onClick={onOpenFighterManager} className="right-container">
-                    <h1>Bestehendes<br />Turnier öffnen</h1>
+                <div onClick={onOpenFighterManager} className="right-container"> {/* Div-Container für den rechten Bereich */}
+                    <h1>Bestehendes<br />Turnier öffnen</h1> {/* Überschrift für die Option, ein bestehendes Turnier zu öffnen */}
                 </div>
             </div>
         </div>
     );
 };
 
-export default HomePage;
+export default HomePage; // Export der HomePage-Komponente als Standardexport
