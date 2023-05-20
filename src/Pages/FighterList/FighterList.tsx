de699da7df99b26d25a98c0e58c216977464c2ba
FerhatYesilkaya <ferhat-20001@hotmail.de>
2023-05-20 23:10:01 +0200

    Revert "Merge branch 'FerhatDev' of https://github.com/Rindersteak/JudoKampfFront into FerhatDev"
    This reverts commit ab09d99a485c93c248dd701274592329488ed419, reversing
    changes made to ce8e9b95ddea3573d25aba8c57db354c135dc554.

--- src/Pages/FighterList/FighterList.tsx
+++ src/Pages/FighterList/FighterList.tsx
@@ -1,31 +1,32 @@
-import React from 'react'; // React importieren
-import FighterEntry from '../FighterEntry/FighterEntry'; // Importieren der FighterEntry-Komponente aus einer anderen Datei
-import { Fighter } from '../../types'; // Importieren des Fighter-Typs aus einer externen Datei
-import './FighterList.css'; // Importieren von Styling für FighterList
+// ParticipantList.tsx
+import React from 'react';
+import FighterEntry from '../FighterEntry/FighterEntry';
+import { Fighter } from '../../types';
+import './FighterList.css';
 
 type Props = {
-    fighters: Fighter[]; // Props-Typ definieren, der ein Array von Kämpfern erwartet
+    fighters: Fighter[];
 };
 
-const FighterList: React.FC<Props> = ({ fighters }) => { // Deklaration der FighterList-Komponente als Funktionskomponente
+const FighterList: React.FC<Props> = ({ fighters }) => {
     return (
-        <div className="listContainer"> {/* Div-Container mit der Klasse "listContainer" */}
-            <h2 className="titleStyle">Teilnehmerliste</h2> {/* Überschrift für die Teilnehmerliste */}
-            <div className="entryList"> {/* Div-Container für die Einträge */}
-                <div className="entryStyle headerStyle"> {/* Div-Container für die Überschriftszeile */}
-                    <span className="nameStyle">Name</span> {/* Spalte für den Namen */}
-                    <span className="clubStyle">Verein</span> {/* Spalte für den Verein */}
+        <div className="listContainer">
+            <h2 className="titleStyle">Teilnehmerliste</h2>
+            <div className="entryList">
+                <div className="entryStyle headerStyle">
+                    <span className="nameStyle">Name</span>
+                    <span className="clubStyle">Verein</span>
                 </div>
-                {fighters.map((fighter, index) => ( // Iteration über die Kämpfer im Array und Rendern von FighterEntry-Komponenten für jeden Kämpfer
-                    <FighterEntry key={index} fighter={fighter} /> // FighterEntry-Komponente mit dem Kämpfer als Prop
+                {fighters.map((fighters, index) => (
+                    <FighterEntry key={index} fighter={fighters} />
                 ))}
                 {/* Testeinträge */}
-                {Array.from({ length: 10 }).map((_, index) => ( // Erstellen von 10 Testeinträgen
-                    <FighterEntry key={`test-${index}`} fighter={{ firstName: 'Vorname', lastName: 'Nachname', club: 'Beispielverein', regionalAssociation: '', birthDate: '2023-05-17', weight: 0 }} /> // FighterEntry-Komponente mit festen Werten als Prop
+                {Array.from({ length: 10 }).map((_, index) => (
+                    <FighterEntry key={`test-${index}`} fighter={{ firstName: 'Vorname', lastName: 'Nachname', club: 'Beispielverein', regionalAssociation: '', birthDate: '2023-05-17', weight: 0 }} />
                 ))}
             </div>
         </div>
     );
 };
 
-export default FighterList; // Export der FighterList-Komponente als Standardexport
+export default FighterList;
