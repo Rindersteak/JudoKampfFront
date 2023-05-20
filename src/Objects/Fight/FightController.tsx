791a7a0976740f7a553e797322b9d8ef06dfb5da
FerhatYesilkaya <ferhat-20001@hotmail.de>
2023-05-20 23:48:10 +0200

    test

--- src/Objects/Fight/FightController.tsx
+++ src/Objects/Fight/FightController.tsx
@@ -1,2 +1,2 @@
-/* Fight-Controller zur Verarbeitung von Anfragen und Ausführung von Aktionen für Fight-Objekte */
+/* Fight-Controller zur Verarbeitung von Anfragen und Ausführung von Aktionen für Fight-Objekte */ //test
 export {} 
