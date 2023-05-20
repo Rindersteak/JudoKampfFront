45d7e5be0f765b8d414eaeb74e919d4a80275e00
FerhatYesilkaya <ferhat-20001@hotmail.de>
2023-05-20 23:10:31 +0200

    Revert "PUSH FORCE"
    This reverts commit ce8e9b95ddea3573d25aba8c57db354c135dc554.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from '../Pages/HomePage/HomePage';
 import FighterManager from '../Pages/FighterManager/FighterManager';
-import TournamentForm from '../Pages/TournamentForm/TournamentForm';
 import SelectedTournament from '../selectedTournament';
 import Modal from '../Modal/Modal';
 import './App.css';
@@ -10,18 +9,16 @@ import './App.css';
 const App: React.FC = () => {
     const [modalOpen, setModalOpen] = useState(false);
 
-    const handleOpenTournamentForm = () => {
-        setModalOpen(true);
-    };
-
     const handleOpenFighterManager = () => {
         setModalOpen(true);
     };
+
     return (
         <Router>
             <div className="container">
                     <Routes>
-                        <Route path="/" element={<HomePage onOpenTournamentForm={handleOpenTournamentForm} onOpenFighterManager={handleOpenFighterManager} />} />
+                        <Route path="/" element={<HomePage onOpenFighterManager={handleOpenFighterManager} />} />
+                        <Route path="/selected-tournament" element={<SelectedTournament />} />
                     </Routes>
                     {modalOpen &&
                         <Modal onClose={() => setModalOpen(false)}>
