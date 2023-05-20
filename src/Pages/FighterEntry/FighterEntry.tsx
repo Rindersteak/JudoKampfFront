8cee7d27339b743fb5a102bf2605df5f624f72e0
FerhatYesilkaya <ferhat-20001@hotmail.de>
2023-05-20 22:13:24 +0200

    added comments

--- src/Pages/FighterEntry/FighterEntry.tsx
+++ src/Pages/FighterEntry/FighterEntry.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { Fighter } from '../../types';
-import './FighterEntry.css';
+import React from 'react'; // React importieren
+import { Fighter } from '../../types'; // Importieren des Fighter-Typs aus einer externen Datei
+import './FighterEntry.css'; // Importieren von Styling für FighterEntry
 
 type Props = {
-    fighter: Fighter;
+    fighter: Fighter; // Props-Typ definieren, der einen Kämpfer erwartet
 };
 
-const FighterEntry: React.FC<Props> = ({ fighter }) => {
+const FighterEntry: React.FC<Props> = ({ fighter }) => { // Deklaration der FighterEntry-Komponente als Funktionskomponente
     return (
-        <div className="participantEntry">
-            <span className="name">{fighter.firstName} {fighter.lastName}</span>
-            <span className="club">{fighter.club}</span>
+        <div className="participantEntry"> {/* Div-Container mit der Klasse "participantEntry" */}
+            <span className="name">{fighter.firstName} {fighter.lastName}</span> {/* Anzeige des Vor- und Nachnamens des Kämpfers */}
+            <span className="club">{fighter.club}</span> {/* Anzeige des Clubs des Kämpfers */}
         </div>
     );
 };
 
-export default FighterEntry;
+export default FighterEntry; // Export der FighterEntry-Komponente als Standardexport
