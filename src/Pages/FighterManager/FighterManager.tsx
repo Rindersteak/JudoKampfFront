6ee8ee403847babe78fdbdc916b1863a03838141
FerhatYesilkaya <ferhat-20001@hotmail.de>
2023-05-20 22:22:54 +0200

    edkwklawe

--- src/Pages/FighterManager/FighterManager.tsx
+++ src/Pages/FighterManager/FighterManager.tsx
@@ -1,30 +1,27 @@
-// FighterManager.tsx
+import React from 'react'; // React importieren
+import FighterForm from '../FighterForm/FighterForm'; // Importieren der FighterForm-Komponente aus einer anderen Datei
+import FighterList from '../FighterList/FighterList'; // Importieren der FighterList-Komponente aus einer anderen Datei
+import { Fighter } from '../../types'; // Importieren des Fighter-Typs aus einer externen Datei
 
-import React from 'react';
-import FighterForm from '../FighterForm/FighterForm';
-import FighterList from '../FighterList/FighterList';
-import { Fighter } from '../../types';
+import './FighterManager.css'; // Importieren von Styling für FighterManager
 
-import './FighterManager.css';
+const FighterManager: React.FC = () => { // Deklaration der FighterManager-Komponente als Funktionskomponente
+    const [fighters, setFighters] = React.useState<Fighter[]>([]); // Verwaltung des Zustands der Kämpfer mit dem useState-Hook
 
-const FighterManager: React.FC = () => {
-    const [fighters, setFighters] = React.useState<Fighter[]>([]);
-
-    const addFighter = (fighter: Fighter) => {
-        setFighters([fighter, ...fighters]);
+    const addFighter = (fighter: Fighter) => { // Funktion zum Hinzufügen eines Kämpfers
+        setFighters([fighter, ...fighters]); // Hinzufügen des neuen Kämpfers zum Array der Kämpfer im Zustand
     };
 
     return (
-        <div className="innerContainer">
-            <div className="formContainer">
-                <FighterForm onAddFighter={addFighter} />
+        <div className="innerContainer"> {/* Div-Container mit der Klasse "innerContainer" */}
+            <div className="formContainer"> {/* Div-Container für das Formular */}
+                <FighterForm onAddFighter={addFighter} /> {/* FighterForm-Komponente mit der onAddFighter-Funktion als Prop */}
             </div>
-            <div className="listContainer">
-                <FighterList fighters={fighters} />
+            <div className="listContainer"> {/* Div-Container für die Teilnehmerliste */}
+                <FighterList fighters={fighters} /> {/* FighterList-Komponente mit den Kämpfern im Zustand als Prop */}
             </div>
         </div>
     );
 };
 
-
-export default FighterManager;
+export default FighterManager; // Export der FighterManager-Komponente als Standardexport
