7504fde8fc5e8faa28ed5582949a46bce5efed41
Lukas <fuckyou>
2023-05-21 10:29:23 +0200

    fixed a bug

--- public/main.js
+++ public/main.js
@@ -5,32 +5,31 @@ const url = require('url');
 let win;
 
 function createWindow() {
-    win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true,
-        },
-    });
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
 
-    win.loadFile(path.join(__dirname, 'build', 'index.html'));
+  win.loadURL('http://localhost:3000'); // URL des React-Entwicklungsmodus-Servers
 
-    win.on('closed', () => {
-        win = null;
-    });
+  win.on('closed', () => {
+    win = null;
+  });
 }
-
 
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('activate', () => {
-    if (win === null) {
-        createWindow();
-    }
+  if (win === null) {
+    createWindow();
+  }
 });
