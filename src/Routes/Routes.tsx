375aec0fd248e305164938f36e2f419497aa35b9
FerhatYesilkaya <ferhat-20001@hotmail.de>
2023-05-21 13:25:53 +0200

    tournamentDetails added

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -5,9 +5,10 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from '../Pages/HomePage/HomePage';
 import TournamentForm from '../Pages/TournamentForm/TournamentForm';
 import FighterManager from '../Pages/FighterManager/FighterManager';
+import TournamentDetails from '../Pages/TournamentDetails/TournamentDetails';
 
 interface AppRoutesProps {
-    onOpenTournamentForm: () => void;
+    onOpenTournamentForm: () => void;   
     onOpenFighterManager: () => void;
 }
 
@@ -16,6 +17,7 @@ const AppRoutes: React.FC<AppRoutesProps> = ({ onOpenTournamentForm, onOpenFight
         <Route path="/" element={<HomePage onOpenTournamentForm={onOpenTournamentForm} onOpenFighterManager={onOpenFighterManager} />} />
         <Route path="/tournament-form" element={<TournamentForm />} />
         <Route path="/fighter-manager" element={<FighterManager />} />
+        <Route path="/tournament-details" element={<TournamentDetails />} />
     </Routes>
 );
 
